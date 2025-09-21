1f3e01ed67810d116e46a996d734cadcca8458ee
haerulr2 <haerulr2@gmail.com>
2025-09-21 15:14:43 +0000

    Refactor Sidebar component by removing unused Divider import and adjusting class names for improved styling consistency.

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,7 +1,6 @@
 "use client";
 import React from "react";
 import { Bolt, ClipboardClock, Cpu, Disc2, LayoutDashboard, Webhook, Workflow } from 'lucide-react';
-import { Divider } from "@heroui/react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
@@ -17,12 +16,12 @@ const Sidebar = () => {
   ];
 
   return (
-    <div className="sidebar max-w-[300px] w-full h-screen py-4 px-2 bg-zinc-800 dark:bg-zinc-900 text-white">
-      <div className="logo mb-4 flex items-center gap-2 text-2xl font-bold">
+    <div className="sidebar max-w-[300px] w-full h-screen py-4 px-2 bg-zinc-800 !text-white">
+      <div className="mb-4 flex items-center gap-2 text-2xl font-bold">
         <Disc2 />
         <h2>Haerulr2</h2>
       </div>
-      <Divider />
+
       <ul className="mt-4 space-y-2">
         {menuItems.map(({ href, icon, label }) => {
           const isActive = pathname === href;
